2b664a9ce08a3095db43edb9d0b0403af8461964
Krabik_356 <bereza3622@gmail.com>
2026-04-25 15:12:52 +0300

    Зареєстрував хендлер логіну у роутері.

--- services/auth_service/cmd/main.go
+++ services/auth_service/cmd/main.go
@@ -57,6 +57,7 @@ func (s *Server) Run() {
 	//s.router.Use(s.handler.CORS) ------------------------------------------UNCOMMENT!!!
 	s.router.Use(s.handler.LoggerMiddleware)
 	s.router.Post("/register", s.handler.Register)
+	s.router.Post("/login", s.handler.Login)
 	//register handlers
 	s.serverLogger.Info("server started",
 		zap.String("start_time", time.Now().Format(time.RFC3339)),
