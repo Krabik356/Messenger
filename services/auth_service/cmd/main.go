504c8df34d6328844a2cffcd8fa0e2e433109c87
Krabik_356 <bereza3622@gmail.com>
2026-04-25 11:28:55 +0300

    Створив структуру серверу з її базовими початковими складовими, зробив grasefull shutdown

--- services/auth_service/cmd/main.go
+++ services/auth_service/cmd/main.go
@@ -1,5 +1,61 @@
 package main
 
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	server *http.Server
+	router *chi.Mux
+	ctx    context.Context
+}
+
+func NewServer(ctx context.Context) *Server {
+	router := chi.NewRouter()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	return &Server{
+		server: server,
+		router: router,
+		ctx:    ctx,
+	}
+}
+
+func (s *Server) Run(ctx context.Context) {
+	//register handlers
+	if err := s.server.ListenAndServe(); err != nil {
+		//log
+		panic(err)
+	}
+}
+
+func (s *Server) Close() {
+	ctxTime, stop := context.WithTimeout(context.Background(), 5*time.Second)
+	defer stop()
+	if err := s.server.Shutdown(ctxTime); err != nil {
+		//log
+	}
+	//close everything
+}
+
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	server := NewServer(ctx)
+
+	//log about start
+	<-ctx.Done()
+	//log about stop
+
+	server.Close()
 
 }
